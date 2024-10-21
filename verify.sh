f5966f9188897cb62e67f93d35e9bc7a33eb4e5f
OmarMesqq <omarmsqt@gmail.com>
2024-10-21 15:49:40 -0300

    test: :test_tube: update verifier script

--- verify.sh
+++ verify.sh
@@ -1,41 +1,36 @@
 #!/bin/sh
 
-echo "Line Count: (-l)"
-echo "wc output"
-wc -l test.txt
-echo "-------------------------"
-echo "Lua script output"
-lua main.lua -l test.txt
-echo "-------------------------"
+compare_outputs() {
+    metric=$1
+    wc_command=$2
+    lua_command=$3
 
-echo "Word Count: (-w)"
-echo "wc output"
-wc -w test.txt
-echo "-------------------------"
-echo "Lua script output"
-lua main.lua -w test.txt
-echo "-------------------------"
+    echo "$metric Count: ($wc_command)"
+    echo "wc output:"
+    wc_output=$(eval "$wc_command")
+    echo "$wc_output"
+    echo "-------------------------"
+    echo "Lua script output:"
+    lua_output=$(eval "$lua_command")
+    echo "$lua_output"
+    echo "-------------------------"
 
-echo "Byte Count: (-c)"
-echo "wc output"
-wc -c test.txt
-echo "-------------------------"
-echo "Lua script output"
-lua main.lua -c test.txt
-echo "-------------------------"
+    if [ "$wc_output" = "$lua_output" ]; then
+        echo -e "\e[32m$metric test passed\e[0m"
+    else
+        echo -e "\e[31m$metric test failed: wc output '$wc_output' does not match lua output '$lua_output'\e[0m"
+        exit 1
+    fi
+}
 
-echo "Character Count: (-m)"
-echo "wc output"
-wc -m test.txt
-echo "-------------------------"
-echo "Lua script output"
-lua main.lua -m test.txt
-echo "-------------------------"
+# Line Count
+compare_outputs "Line" "wc -l test.txt" "lua main.lua -l test.txt"
 
-echo "Piping filename:" 
-echo "wc output"
-cat test.txt | wc
-echo "-------------------------"
-echo "Lua script output"
-cat test.txt | lua main.lua 
-echo "-------------------------"
+# Word Count
+compare_outputs "Word" "wc -w test.txt" "lua main.lua -w test.txt"
+
+# Byte Count
+compare_outputs "Byte" "wc -c test.txt" "lua main.lua -c test.txt"
+
+# Character Count
+compare_outputs "Character" "wc -m test.txt" "lua main.lua -m test.txt"
