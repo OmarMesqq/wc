4f4cb66884579c8809ca48b44aaaeef37564a651
OmarMesqq <omarmsqt@gmail.com>
2024-10-21 18:35:28 -0300

    test: :white_check_mark: add File piping test

--- verify.sh
+++ verify.sh
@@ -18,7 +18,7 @@ compare_outputs() {
     if [ "$wc_output" = "$lua_output" ]; then
         echo -e "\e[32m$metric test passed\e[0m"
     else
-        echo -e "\e[31m$metric test failed: wc output '$wc_output' does not match lua output '$lua_output'\e[0m"
+        echo -e "\e[31m$metric test failed!\nwc output:\n'$wc_output'\ndid not match lua output:\n'$lua_output'\e[0m"
         exit 1
     fi
 }
@@ -34,3 +34,6 @@ compare_outputs "Byte" "wc -c test.txt" "lua main.lua -c test.txt"
 
 # Character Count
 compare_outputs "Character" "wc -m test.txt" "lua main.lua -m test.txt"
+
+# File piping test
+compare_outputs "File pipe" "cat test.txt | wc" "cat test.txt | lua main.lua"
